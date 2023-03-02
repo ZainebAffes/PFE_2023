2a75b9f05873c6869f3301aa2b73f6e2eac43f06
ZainebAffes <zaineb.affes20@gmail.com>
2023-03-02 13:23:46 +0100

    template

---
+++ projet/workflowDemande-core/src/main/java/com/csys/workflowDemande/config/SwaggerConfProd.java
@@ -0,0 +1,11 @@
+package com.csys.workflowDemande.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class SwaggerConfProd {
+
+}
---
+++ projet/workflowDemande-core/src/main/java/com/csys/workflowDemande/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.csys.workflowDemande.web.rest.vm;
